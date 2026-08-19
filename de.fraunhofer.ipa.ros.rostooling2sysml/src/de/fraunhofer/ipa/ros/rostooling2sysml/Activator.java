812ce3640fa035895e7d9b473d88a3e356d8caa6
Eshan <eshan.savla@ipa.fraunhofer.de>
2026-08-19 13:44:35 +0200

    switched to using XtextActivator

--- de.fraunhofer.ipa.ros.rostooling2sysml/src/de/fraunhofer/ipa/ros/rostooling2sysml/Activator.java
+++ de.fraunhofer.ipa.ros.rostooling2sysml/src/de/fraunhofer/ipa/ros/rostooling2sysml/Activator.java
@@ -1,42 +1,7 @@
-//****************************************************************************/
-//  Copyright (c) 2022-2026 The CORESENSE Consortium.                        //
-//  Licensed under the Apache License, Version 2.0                           //
-//****************************************************************************/
-
 package de.fraunhofer.ipa.ros.rostooling2sysml;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
-import org.osgi.framework.BundleContext;
+import de.fraunhofer.ipa.rossystem.xtext.ui.internal.XtextActivator;
 
-/**
- * The activator class controls the plug-in life cycle
- */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends XtextActivator {
 
-    public static final String PLUGIN_ID = "de.fraunhofer.ipa.ros.rostooling2sysml"; //$NON-NLS-1$
-
-    private static Activator plugin;
-
-    public Activator() {
-    }
-
-    @Override
-    public void start(BundleContext context) throws Exception {
-        super.start(context);
-        plugin = this;
-    }
-
-    @Override
-    public void stop(BundleContext context) throws Exception {
-        plugin = null;
-        super.stop(context);
-    }
-
-    public static Activator getDefault() {
-        return plugin;
-    }
-
-    public static Activator getContext() {
-        return plugin;
-    }
 }
--- de.fraunhofer.ipa.ros.sysml2rostooling/src/de/fraunhofer/ipa/ros/sysml2rostooling/Activator.java
+++ de.fraunhofer.ipa.ros.sysml2rostooling/src/de/fraunhofer/ipa/ros/sysml2rostooling/Activator.java
@@ -1,22 +1,7 @@
-// Copyright (c) 2022-2026 The CORESENSE Consortium. Apache License 2.0.
-
 package de.fraunhofer.ipa.ros.sysml2rostooling;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
+import de.fraunhofer.ipa.rossystem.xtext.ui.internal.XtextActivator;
 
-public class Activator implements BundleActivator {
-    private static BundleContext context;
-    
-    public static BundleContext getContext() { return context; }
-    
-    @Override
-    public void start(BundleContext bundleContext) throws Exception {
-        Activator.context = bundleContext;
-    }
-    
-    @Override
-    public void stop(BundleContext bundleContext) throws Exception {
-        Activator.context = null;
-    }
+public class Activator extends XtextActivator {
+
 }
